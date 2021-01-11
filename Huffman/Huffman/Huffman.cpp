27dfb4a754c68ff3de69552fe0f76ae1eb47edb9
Noam shushan <noam8shu@gmail.com>
2021-01-11 16:52:19 +0200

    var 3

--- Huffman/Huffman/Huffman.cpp
+++ Huffman/Huffman/Huffman.cpp
@@ -2,26 +2,46 @@
 
 Huffman::Huffman()
 {
-
-}
-
-Huffman::~Huffman()
-{
-	delMem(root);
-}
-
-void Huffman::build(std::string word)
-{
 	_word = word;
 	std::string freqTable = countCharWithFreq(word);
-	buildQueue(freqTable);
-	buildTree();
+	for (std::string::size_type i = 0; i < freqTable.size(); i++) 
+	{
+		if (freqTable[i] != ' ' && i < freqTable.size() + 2)
+		{
+			if (isalnum(freqTable[i]) && freqTable[i + 1] == '-' && isalnum(freqTable[i+2])) 
+			{
+				char c = freqTable[i];
+				int freq = static_cast<int>(freqTable[i + 2] - '0');
+				HuffmanNode* node = new HuffmanNode(freq, c);
+				tree.push(node);
+			}
+		}
+	}
+	for(int i = 0; i < tree.size() + 1; i++)
+	{
+		HuffmanNode* min1 = tree.top();
+		tree.pop();
+		HuffmanNode* min2 = tree.top();
+		tree.pop();
+		HuffmanNode* ptr = new HuffmanNode(min1->frequency + min2->frequency, 0, false);
+		ptr->right = min2;
+		ptr->left = min1;
+		min1->father = min2->father = ptr;
+		tree.push(ptr);
+	}
+	root = tree.top();
+	tree.pop();
 	setTreeStruct(root);
 	setNodeCode(root, "");
 	setCode(word);
 }
 
 std::string Huffman::encode()
+{
+	delMem(root);
+}
+
+std::string Huffman::encod()
 {
 	std::stringstream out;
 	std::string freqTable = countCharWithFreq(_word);
@@ -39,30 +59,21 @@ std::string Huffman::decode(int numOfDif, std::string difLetters,
 	bulidTreeFromStruct(tempRoot, wordStruct, 0);
 	setNodeCode(tempRoot, "");
 	setLetters(tempRoot, difLetters, 0);
-	
+
 	std::stringstream out;
-	int i = 0, codeSizeOfLetter = 1;
-	while(i < code.size())
+	for(int i = 1; i < numOfDif; i++) 
 	{
-		std::string sub = code.substr(i, codeSizeOfLetter);
-		char res = 0;
-		if (codeSizeOfLetter >= numOfDif) 
+		for (int j = 0; j < code.size() - numOfDif; j++)
 		{
-			return "Erorr";
+			std::string sub = code.substr(j, i);
+			char res = 0;
+			if (getLetter(tempRoot, sub, &res))
+				out << res;
 		}
-		if (getLetter(tempRoot, sub, &res)) 
-		{
-			out << res;
-			i += codeSizeOfLetter;
-			codeSizeOfLetter = 1;
-		}
-		else 
-		{
-			codeSizeOfLetter++;
-		}		
 	}
-	delMem(tempRoot);
 	
+	delMem(tempRoot);
+
 	return out.str();
 }
 
@@ -105,8 +116,8 @@ void Huffman::setLetters(HuffmanNode* node, std::string difLetters, int ind)
 {
 	if (ind == difLetters.size())
 		return;
-	
-	if (node->isLeaf) 
+
+	if (node->isLeaf)
 	{
 		node->str = difLetters[ind];
 		_count++;
@@ -120,14 +131,13 @@ void Huffman::bulidTreeFromStruct(HuffmanNode* node, std::string wordStruct, int
 {
 	if (ind >= wordStruct.size())
 		return;
-	
 	if (wordStruct[ind] == '0') 
 	{
 		node->left = new HuffmanNode(0, 0, false);
 		node->right = new HuffmanNode(0, 0, false);
 		bulidTreeFromStruct(node->left, wordStruct, ind + 1);
 	}
-	if (wordStruct[ind] == '1') 
+	if (wordStruct[ind] == '1')
 	{
 		node->isLeaf = true;
 		return;
@@ -139,16 +149,16 @@ void Huffman::setNodeCode(HuffmanNode* node, std::string nodeCode)
 {
 	if (!node)
 		return;
-	
-	if (node->isLeaf) 
+
+	if (node->isLeaf)
 		node->code = nodeCode;
-	
+
 	setNodeCode(node->left, nodeCode + "0");
 	setNodeCode(node->right, nodeCode + "1");
 }
 
 void Huffman::getCode(HuffmanNode* node, char letter, std::string* res)
-{	
+{
 	if (node->isLeaf)
 	{
 		if (node->str == letter)
@@ -160,7 +170,7 @@ void Huffman::getCode(HuffmanNode* node, char letter, std::string* res)
 	}
 	if (node->left)
 		getCode(node->left, letter, res);
-	
+
 	if (node->right)
 		getCode(node->right, letter, res);
 }
@@ -176,13 +186,13 @@ bool Huffman::getLetter(HuffmanNode* node, std::string nodeCode, char* res)
 		}
 		return false;
 	}
-	return getLetter(node->left, nodeCode, res) 
+	return getLetter(node->left, nodeCode, res)
 		|| getLetter(node->right, nodeCode, res);
 }
 
 void Huffman::setCode(std::string word)
 {
-	for(int i = 0; i < word.size(); i++)
+	for (int i = 0; i < word.size(); i++)
 	{
 		std::string res = "";
 		getCode(root, word[i], &res);
@@ -192,13 +202,13 @@ void Huffman::setCode(std::string word)
 
 void Huffman::setTreeStruct(HuffmanNode* node)
 {
-	if (node->isLeaf) 
+	if (node->isLeaf)
 	{
 		_treeStruct += "1";
 		_letters += node->str;
 		return;
 	}
-	else 
+	else
 	{
 		_treeStruct += "0";
 		setTreeStruct(node->left);
@@ -249,14 +259,14 @@ std::string Huffman::countCharWithFreq(std::string str)
 
 void Huffman::delMem(HuffmanNode* node)
 {
-	if (node == NULL)	
+	if (node == NULL)
 		return;
 
 	if (node->left != NULL)
 		delMem(node->left);
-	
+
 	if (node->right != NULL)
 		delMem(node->right);
-	
+
 	delete node;
 }
